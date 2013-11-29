86e581ab88fbe4654ed596cc56fbf7400b6c73dc
Ken Swain <ken@kenswain.com>
2013-11-29 02:12:21 -0600

    Appearance changes  

--- ram-clean.sh
+++ ram-clean.sh
@@ -3,6 +3,9 @@ echo "Starting Purge"
 purge
 echo "Starting Permissions Repair"
 diskutil repairPermissions /Volumes/Macintosh\ HD
+echo ""
+echo ""
+echo ""
 echo "RAM now clean"
 echo "..."
 echo "You may now close the terminal"
